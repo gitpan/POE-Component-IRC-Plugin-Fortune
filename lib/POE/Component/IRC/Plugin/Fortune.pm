27d2c611d676027e5abfe9ad8ac1552f5567d8f7
Zoffix Znet <zoffixcpan@gmail.com>
2013-11-15 23:11:12 +0000

    Import of ZOFFIX/POE-Component-IRC-Plugin-Fortune-0.0102 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC-Plugin-Fortune
    gitpan-cpan-version:      0.0102
    gitpan-cpan-path:         ZOFFIX/POE-Component-IRC-Plugin-Fortune-0.0102.tar.gz
    gitpan-cpan-author:       ZOFFIX
    gitpan-cpan-maturity:     released

--- lib/POE/Component/IRC/Plugin/Fortune.pm
+++ lib/POE/Component/IRC/Plugin/Fortune.pm
@@ -3,7 +3,7 @@ package POE::Component::IRC::Plugin::Fortune;
 use warnings;
 use strict;
 
-our $VERSION = '0.0101';
+our $VERSION = '0.0102';
 
 use base 'POE::Component::IRC::Plugin::BaseWrap';
 
@@ -35,6 +35,8 @@ sub _message_into_response_event { 'fortune' }
 
 1;
 __END__
+
+=encoding utf8
 
 =head1 NAME
 
